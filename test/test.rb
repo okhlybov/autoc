664087c4ac26f89c421e5c52ca4d0ed1e4b752d9
fougas <fougas@localhost>
2014-04-18 15:59:14 +0600

    Made Vector collection to (hopefully) support value types. Started a new test set. Fixed a few bugs exposed by new tests.

--- test/test.rb
+++ test/test.rb
@@ -1,27 +1,15 @@
 require "autoc"
 
+ValueType = {
+  :type => :ValueType,
+  :forward => %$#include "test2.h"$,
+  :ctor => :ValueTypeCtor,
+  :dtor => :ValueTypeDtor,
+  :copy => :ValueTypeCopy,
+  :equal => :ValueTypeEqual,
+  :less => :ValueTypeLess,
+}
 
-include CodeBuilder
-include AutoC
-
-
-Int = {:type=>"int"}
-PChar = {:type=>"const char*", :equal=>"PCharEqual", :identify=>"PCharHash"}
-Box = {:type=>"Box*", :forward=>"typedef struct Box Box;", :copy=>"BoxCopy", :equal=>"BoxEqual", :identify=>"BoxHash", :ctor=>"BoxNew", :dtor=>"BoxDestroy"}
-
-
-IntVector = Vector.new(:IntVector, Int)
-
-
-CModule.generate!(:Test) do |m|
-  m << IntVector
-  m << Vector.new(:BoxVector, Box)
-  m << List.new(:BoxList, Box)
-  m << AutoC::Queue.new(:PCharQueue, PChar)
-  m << HashSet.new(:BoxSet, Box)
-  m << HashSet.new(:IntSet, Int)
-  m << HashSet.new(:PCharSet, PChar)
-  m << HashMap.new(:Box2BoxMap, Box, Box)
-  m << HashMap.new(:PChar2IntMap, PChar, Int)
-  #m << HashMap.new(:PChar2IntVectorMap, PChar, IntVector)
+CodeBuilder::CModule.generate!(:Test2) do |m|
+  m << AutoC::Vector.new(:ValueTypeVector, ValueType)
 end
