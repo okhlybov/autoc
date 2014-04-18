a778da280b6af9367a34eeb335dc21c63bac33cc
Oleg A. Khlybov <fougas@mail.ru>
2014-04-18 16:05:16 +0600

    Added missing test header.

---
+++ test/test.h
@@ -0,0 +1,34 @@
+#ifndef test_h
+#define test_h
+
+
+typedef struct ValueType ValueType;
+
+
+struct ValueType {
+  int size;
+  void* block;
+};
+
+
+#define ValueTypeCtor(obj) _ValueTypeCtor(&obj)
+void _ValueTypeCtor(ValueType*);
+
+
+#define ValueTypeDtor(obj) _ValueTypeDtor(&obj)
+void _ValueTypeDtor(ValueType*);
+
+
+#define ValueTypeCopy(dst, src) _ValueTypeCopy(&dst, &src)
+void _ValueTypeCopy(ValueType*, ValueType*);
+
+
+#define ValueTypeEqual(lt, rt) _ValueTypeEqual(&lt, &rt)
+int _ValueTypeEqual(ValueType*, ValueType*);
+
+
+#define ValueTypeLess(lt, rt) _ValueTypeLess(&lt, &rt)
+int _ValueTypeLess(ValueType*, ValueType*);
+
+
+#endif
