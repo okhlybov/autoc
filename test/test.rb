e8967556bf4d345d487e1354e40503b3f267eb06
fougas <fougas@localhost>
2014-04-24 16:52:49 +0600

    Merged Module and CModule classes.

--- test/test.rb
+++ test/test.rb
@@ -11,7 +11,7 @@ ValueType = {
   :identify => :ValueTypeIdentify,
 }
 
-AutoC::CModule.generate!(:Test) do |m|
+AutoC::Module.generate!(:Test) do |m|
   m << AutoC::Vector.new(:ValueTypeVector, ValueType)
   m << AutoC::List.new(:ValueTypeList, ValueType)
   m << AutoC::Queue.new(:ValueTypeQueue, ValueType)
