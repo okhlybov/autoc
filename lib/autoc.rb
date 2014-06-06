0c504b0563cb3dea75adbc911e4efea77e4fa833
fougas <fougas@localhost>
2014-06-06 18:03:45 +0600

    Doc updates. Version bump.

--- lib/autoc.rb
+++ lib/autoc.rb
@@ -2,14 +2,26 @@
 
 *AutoC* is a host of Ruby modules related to automatic C source code generation.
 
-. Generic C multi-source module generator.
-. Strongly-typed data structure generators akin the C++ STL container classes.
+. {AutoC::Code} - generic C multi-source module generator.
+. {AutoC::Collection} - strongly-typed data structure generators similar
+to the C++ STL container classes.
 
 == Versioning scheme
 
+AutoC adheres to simple major.minor versioning scheme.
+
+Change in major number states incompatible changes in the code
+while change in minor number states (rather) small incremental changes that
+should not normally break things.
+
+That said, release 1.0 is the _first_ release of version 1 which is considered beta, not stable.
+_Note that it is not necessary stable or feature complete._
+Releases 1.1+ will present incremental improvements, bugfixes, documentation updates etc. to version 1.
+Should the major incompatible changes be made, the new release 2.0 will be introduced and so forth.
+
 =end
 module AutoC
-  VERSION = "0.9"
+  VERSION = "1.0"
 end # AutoC
 
 
