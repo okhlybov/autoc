8ecc70b2f2fdbbda2de142bd1d6b6a571bb7a510
Oleg A. Khlybov <fougas@mail.ru>
2016-02-29 17:10:11 +0500

    Seeded the test value type. Seede the value type vector test.

---
+++ test/test_value_vector.rb
@@ -0,0 +1,40 @@
+require "value"
+
+
+type_test(AutoC::Vector, :ValueVector, Value) do
+  
+setup %~#{type} t;~
+cleanup %~#{dtor}(&t);~
+
+  test :createSmallest, %~
+    #{ctor}(&t, 1);
+    TEST_EQUAL( #{size}(&t), 1 );
+  ~
+    
+  test :createLarge, %~
+    #{ctor}(&t, 1024);
+    TEST_EQUAL( #{size}(&t), 1024 );
+  ~
+    
+setup %~#{type} t1, t2; #{element.type} e;~
+cleanup %~#{dtor}(&t1); #{dtor}(&t2);~
+
+  test :equal, %~
+    int i, c = 3;
+    #{ctor}(&t1, c);
+    #{ctor}(&t2, c);
+    TEST_TRUE( #{equal}(&t1, &t2) );
+    for(i = 0; i < c; ++i) {
+      #{element.ctorEx}(e, i);
+      #{set}(&t1, i, e);
+      #{set}(&t2, i, e);
+      #{element.dtor}(e);
+    }
+    TEST_TRUE( #{equal}(&t1, &t2) );
+    #{element.ctorEx}(e, -1);
+    #{element.dtor}(e);
+    #{set}(&t1, 0, e);
+    TEST_FALSE( #{equal}(&t1, &t2) );
+  ~
+  
+end
