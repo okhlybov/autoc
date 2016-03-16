4601f08058918755d9d6cf80e962e74af0c5ef58
fougas <fougas@localhost>
2016-03-16 16:31:41 +0500

    Got rid of the old test suite.

--- test/test.rb
+++ test/test.rb
@@ -1,516 +1,158 @@
+=begin
+
+This is the AutoC automatic unit test generator.
+Usage instruction:
+
+1. Generate get test's source code (test_auto.c and test_auto.h):
+> ruby -I . -I ../lib test.rb
+
+2. Compile the generated code:
+> cc test_auto.c
+  
+3. Run the tests:
+> ./a.out
+
+The code is intended to finish succesfully,
+the process' exit code is zero when all tests are passed
+ano non-zero if there were failed tests.
+
+The compile-time warnings are possible and may be ignored.
+
+The compiled code should also pass the memory leakage tests
+(with Valgrind, Dr.Memory etc.)
+
+=end
+
 require "autoc"
 
-ValueType = {
-  :type => :ValueType,
-  :ctor => :ValueTypeCtor,
-  :dtor => :ValueTypeDtor,
-  :copy => :ValueTypeCopy,
-  :equal => :ValueTypeEqual,
-  :less => :ValueTypeLess,
-  :identify => :ValueTypeIdentify,
-  :forward => %$#include "test.h"$,
-}
-
-PInt = AutoC::Reference.new(:type => :int, :prefix => :Int)
-
-PValueType = AutoC::Reference.new(ValueType)
-
-IntVector = AutoC::Vector.new(:IntVector, :int)
-
-IntSet = AutoC::HashSet.new(:IntSet, :int)
-
-ListIntSet = AutoC::List.new(:ListIntSet, IntSet)
-
-ValueTypeVector = Class.new(AutoC::Vector) do
-  def write_intf(stream)
-    super
-    stream << %$#{extern} void #{test!}(void);$
-  end
+Prologue = Class.new(AutoC::Code) do
   def write_defs(stream)
-    super
-    stream << %$
-      void #{test!}(void) {
-        #{element.type} e1, e2;
-        #{type} c1, c2;
-        #{it} it;
-        
-        #{ctor}(&c1, 3);
-        #{copy}(&c2, &c1);
-        
-        #{resize}(&c1, 5);
-        #{size}(&c1);
-        #{equal}(&c1, &c2);
-        #{resize}(&c1, 3);
-        #{size}(&c1);
-        #{equal}(&c1, &c2);
-        
-        #{within}(&c1, 0);
-        #{within}(&c1, 10);
-        
-        #{element.ctorEx}(&e1, -1);
-        #{element.ctorEx}(&e2, +1);
-        #{set}(&c1, 2, e1);
-        #{set}(&c1, 0, e2);
-        #{element.dtor}(e1);
-        #{element.dtor}(e2);
-        
-        #{itCtor}(&it, &c1);
-        while(#{itMove}(&it)) {
-            #{element.type} e = #{itGet}(&it);
-            #{element.dtor}(e);
-        }
-        
-        #{itCtorEx}(&it, &c2, 0);
-        while(#{itMove}(&it)) {
-            #{element.type} e = #{itGet}(&it);
-            #{element.dtor}(e);
-        }
-    
-        #{sort}(&c1);
-        #{sort}(&c2);
-        
-        #{identify}(&c1);
-        #{identify}(&c2);
-        
-        #{dtor}(&c1);
-        #{dtor}(&c2);
+    stream << %~
+      #include <stdio.h>
+      struct {
+        int total, processed, failed;
+      } tests;
+      int failure;
+      typedef void (*test_func)(void);
+      void run_test(const char* name, test_func func) {
+        fprintf(stdout, "+   %s\\n", name);
+        fflush(stdout);
+        failure = 0;
+        func();
+        if(failure) tests.failed++;
+        tests.processed++;
       }
-    $
+      void print_condition_failure(const char* message, const char* condition, const char* file, int line) {
+        fprintf(stderr, "*** %s : %s (%s:%d)\\n", condition, message, file, line);
+        fflush(stderr);
+        failure = 1;
+      }
+      void print_equality_failure(const char* message, const char* x, const char* y, const char* file, int line) {
+        fprintf(stderr, "*** %s == %s : %s (%s:%d)\\n", x, y, message, file, line);
+        fflush(stderr);
+        failure = 1;
+      }
+      void print_summary(void) {
+        if(tests.failed)
+          fprintf(stdout, "*** Failed %d of %d tests\\n", tests.failed, tests.processed);
+        else
+          fprintf(stdout, "+++ All %d tests passed successfully\\n", tests.processed);
+        fflush(stdout);
+      }
+      #define TEST_MESSAGE(s) fprintf(stderr, "*** %s\\n", s); fflush(stderr);
+      #define TEST_ASSERT(x) if(x) {} else print_condition_failure("evaluated to FALSE", #x, __FILE__, __LINE__) 
+      #define TEST_TRUE(x) if(x) {} else print_condition_failure("expected TRUE but got FALSE", #x, __FILE__, __LINE__) 
+      #define TEST_FALSE(x) if(x) print_condition_failure("expected FALSE but got TRUE", #x, __FILE__, __LINE__)
+      #define TEST_NULL(x) if((x) == NULL) {} else print_condition_failure("expected NULL", #x, __FILE__, __LINE__) 
+      #define TEST_NOT_NULL(x) if((x) == NULL) print_condition_failure("expected not NULL", #x, __FILE__, __LINE__)
+      #define TEST_EQUAL(x, y) if((x) == (y)) {} else print_equality_failure("expected equality", #x, #y, __FILE__, __LINE__)
+      #define TEST_NOT_EQUAL(x, y) if((x) == (y)) print_equality_failure("expected non-equality", #x, #y, __FILE__, __LINE__)
+      #define TEST_EQUAL_CHARS(x, y) if(strcmp(x, y) == 0) {} else print_equality_failure("expected strings equality", #x, #y, __FILE__, __LINE__)
+      #define TEST_NOT_EQUAL_CHARS(x, y) if(strcmp(x, y) == 0) print_equality_failure("expected strings non-equality", #x, #y, __FILE__, __LINE__)
+    ~
   end
-end.new(:ValueTypeVector, ValueType)
+end.new
 
-ValueTypeList = Class.new(AutoC::List) do
-  def write_intf(stream)
-    super
-    stream << %$#{extern} void #{test!}(void);$
-  end
+
+Epilogue = Class.new(AutoC::Code) do
+  def priority; AutoC::Priority::MIN end
   def write_defs(stream)
-    super
-    stream << %$
-      void #{test!}(void) {
-        #{element.type} e1, e2, e3;
-        #{type} c1, c2;
-        #{it} it;
-        
-        #{ctor}(&c1);
-        #{copy}(&c2, &c1);
-        
-        #{equal}(&c1, &c2);
-        #{empty}(&c1);
-        
-        #{itCtor}(&it, &c1);
-        while(#{itMove}(&it)) {
-            #{element.type} e = #{itGet}(&it);
-            #{element.dtor}(e);
-        }
-        
-        #{element.ctorEx}(&e1, -1);
-        #{element.ctorEx}(&e2, +1);
-        
-        #{push}(&c1, e1);
-        #{push}(&c2, e2);
-        #{contains}(&c1, e1);
-        #{contains}(&c2, e1);
-        #{push}(&c1, e2);
-        #{push}(&c2, e1);
-        #{empty}(&c1);
-        
-        #{element.dtor}(e1);
-        #{element.dtor}(e2);
-    
-        e1 = #{peek}(&c1);
-        e2 = #{peek}(&c2);
-        #{element.dtor}(e1);
-        #{element.dtor}(e2);
-        
-        #{itCtor}(&it, &c1);
-        while(#{itMove}(&it)) {
-            #{element.type} e = #{itGet}(&it);
-            #{element.dtor}(e);
-        }
-    
-        #{identify}(&c1);
-        #{identify}(&c2);
-    
-        e1 = #{pop}(&c1);
-        e2 = #{pop}(&c2);
-        #{element.dtor}(e1);
-        #{element.dtor}(e2);
-    
-        #{purge}(&c1);
-        #{purge}(&c2);
-    
-        #{element.ctorEx}(&e1, 3);
-        #{element.ctorEx}(&e2, -3);
-        #{element.copy}(e3, e2);
-        #{push}(&c1, e1);
-        #{push}(&c1, e2);
-        #{push}(&c1, e1);
-        #{push}(&c2, e2);
-        #{push}(&c2, e2);
-        #{push}(&c2, e2);
-        #{replace}(&c2, e3);
-        #{replaceAll}(&c2, e3);
-        #{remove}(&c1, e2);
-        #{remove}(&c1, e1);
-        #{remove}(&c2, e1);
-        #{removeAll}(&c2, e2);
-        #{element.dtor}(e1);
-        #{element.dtor}(e2);
-        #{element.dtor}(e3);
-    
-        #{dtor}(&c1);
-        #{dtor}(&c2);
-      }
-    $
+    total = 0
+    $tests.each {|t| total += t.tests.size}
+    stream << %~int main(int argc, char** argv) {~
+    stream << %~
+      tests.total = #{total};
+      tests.processed = tests.failed = 0;
+    ~
+    $tests.each {|t| t.write_test_calls(stream)}
+    stream << %~print_summary();~
+    stream << %~return tests.failed > 0;}~
   end
-end.new(:ValueTypeList, ValueType)
+end.new
 
-# Queue is a non-strict superset of List so the test case for the latter can be reused as-is
-ValueTypeQueue = Class.new(AutoC::Queue) do
-  def write_intf(stream)
-    super
-    stream << %$#{extern} void #{test!}(void);$
-  end
-  def write_defs(stream)
-    super
-    stream << %$
-      void #{test!}(void) {
-        #{element.type} e1, e2, e3;
-        #{type} c1, c2;
-        #{it} it;
-        
-        #{ctor}(&c1);
-        #{copy}(&c2, &c1);
-        
-        #{equal}(&c1, &c2);
-        #{empty}(&c1);
-        
-        #{itCtor}(&it, &c1);
-        while(#{itMove}(&it)) {
-            #{element.type} e = #{itGet}(&it);
-            #{element.dtor}(e);
-        }
-        
-        #{element.ctorEx}(&e1, -1);
-        #{element.ctorEx}(&e2, +1);
-        
-        #{push}(&c1, e1);
-        #{push}(&c2, e2);
-        #{contains}(&c1, e1);
-        #{contains}(&c2, e1);
-        #{push}(&c1, e2);
-        #{push}(&c2, e1);
-        #{empty}(&c1);
-        
-        #{element.dtor}(e1);
-        #{element.dtor}(e2);
-    
-        e1 = #{peek}(&c1);
-        e2 = #{peek}(&c2);
-        #{element.dtor}(e1);
-        #{element.dtor}(e2);
-        
-        #{itCtor}(&it, &c1);
-        while(#{itMove}(&it)) {
-            #{element.type} e = #{itGet}(&it);
-            #{element.dtor}(e);
-        }
-    
-        #{identify}(&c1);
-        #{identify}(&c2);
-    
-        e1 = #{pop}(&c1);
-        e2 = #{pop}(&c2);
-        #{element.dtor}(e1);
-        #{element.dtor}(e2);
-    
-        #{purge}(&c1);
-        #{purge}(&c2);
-    
-        #{element.ctorEx}(&e1, 3);
-        #{element.ctorEx}(&e2, -3);
-        #{element.copy}(e3, e2);
-        #{push}(&c1, e1);
-        #{push}(&c1, e2);
-        #{push}(&c1, e1);
-        #{push}(&c2, e2);
-        #{push}(&c2, e2);
-        #{push}(&c2, e2);
-        #{replace}(&c2, e3);
-        #{replaceAll}(&c2, e3);
-        #{remove}(&c1, e2);
-        #{remove}(&c1, e1);
-        #{remove}(&c2, e1);
-        #{removeAll}(&c2, e2);
-        #{element.dtor}(e1);
-        #{element.dtor}(e2);
-        #{element.dtor}(e3);
-    
-        #{dtor}(&c1);
-        #{dtor}(&c2);
-      }
-    $
-  end
-end.new(:ValueTypeQueue, ValueType)
 
-ValueTypeSet = Class.new(AutoC::HashSet) do
-  def write_intf(stream)
-    super
-    stream << %$#{extern} void #{test!}(void);$
-  end
-  def write_defs(stream)
-    super
-    stream << %$
-      void #{test!}(void) {
-        #{element.type} e1, e2, e3;
-        #{type} c1, c2, cc1, cc2;
-        #{it} it;
-        
-        #{ctor}(&c1);
-        #{copy}(&c2, &c1);
-        
-        #{itCtor}(&it, &c1);
-        while(#{itMove}(&it)) {
-            #{element.type} e = #{itGet}(&it);
-            #{element.dtor}(e);
-        }
-    
-        #{equal}(&c1, &c2);
-        #{empty}(&c1);
-        #{size}(&c1);
-        
-        #{element.ctorEx}(&e1, -1);
-        #{element.ctorEx}(&e2, +1);
-        #{element.ctorEx}(&e3, 0);
-    
-        #{put}(&c1, e1);
-        #{put}(&c2, e1);
-        #{equal}(&c1, &c2);
-        #{put}(&c1, e2);
-        #{put}(&c2, e3);
-        #{equal}(&c1, &c2);
-        #{contains}(&c1, e1);
-        #{contains}(&c2, e2);
-        {
-            #{element.type} e = #{get}(&c2, e3);
-            #{element.dtor}(e);
-        }
-        #{replace}(&c1, e2);
-    
-        #{put}(&c2, e1);
-        #{put}(&c2, e2);
-        #{put}(&c2, e3);
-        
-        #{element.dtor}(e1);
-        #{element.dtor}(e2);
-        #{element.dtor}(e3);
-        
-        {
-            int i;
-            #{element.type} e;
-            for(i = 0; i < 100; ++i) {
-                #{element.ctorEx}(&e, i);
-                #{put}(&c1, e);
-                #{element.dtor}(e);
-            }
-            for(i = 0; i < 100; i += 2) {
-                #{element.ctorEx}(&e, i);
-                #{remove}(&c1, e);
-                #{element.dtor}(e);
-            }
-            #{itCtor}(&it, &c1);
-            while(#{itMove}(&it)) {
-                #{element.type} e = #{itGet}(&it);
-                #{element.dtor}(e);
-            }
-        }
-        
-        {
-            #{copy}(&cc1, &c1);
-            #{copy}(&cc2, &c2);
-            #{retain}(&cc1, &cc2);
-            #{dtor}(&cc1);
-            #{dtor}(&cc2);
-        }
-        
-        {
-            #{copy}(&cc1, &c1);
-            #{copy}(&cc2, &c2);
-            #{retain}(&cc2, &cc1);
-            #{dtor}(&cc1);
-            #{dtor}(&cc2);
-        }
-    
-        {
-            #{copy}(&cc1, &c1);
-            #{copy}(&cc2, &c2);
-            #{include}(&cc1, &cc2);
-            #{dtor}(&cc1);
-            #{dtor}(&cc2);
-        }
-        
-        {
-            #{copy}(&cc1, &c1);
-            #{copy}(&cc2, &c2);
-            #{include}(&cc2, &cc1);
-            #{dtor}(&cc1);
-            #{dtor}(&cc2);
-        }
-    
-        {
-            #{copy}(&cc1, &c1);
-            #{copy}(&cc2, &c2);
-            #{exclude}(&cc1, &cc2);
-            #{dtor}(&cc1);
-            #{dtor}(&cc2);
-        }
-        
-        {
-            #{copy}(&cc1, &c1);
-            #{copy}(&cc2, &c2);
-            #{exclude}(&cc2, &cc1);
-            #{dtor}(&cc1);
-            #{dtor}(&cc2);
-        }
-    
-        {
-            #{copy}(&cc1, &c1);
-            #{copy}(&cc2, &c2);
-            #{invert}(&cc1, &cc2);
-            #{dtor}(&cc1);
-            #{dtor}(&cc2);
-        }
-        
-        {
-            #{copy}(&cc1, &c1);
-            #{copy}(&cc2, &c2);
-            #{invert}(&cc2, &cc1);
-            #{dtor}(&cc1);
-            #{dtor}(&cc2);
-        }
-    
-        #{identify}(&c1);
-        #{identify}(&c2);
-    
-        #{purge}(&c1);
-        #{dtor}(&c1);
-        #{dtor}(&c2);
-      }
-    $
-  end
-end.new(:ValueTypeSet, ValueType)
+$tests = []
 
-ValueTypeMap = Class.new(AutoC::HashMap) do
-  def write_intf(stream)
-    super
-    stream << %$#{extern} void #{test!}(void);$
-  end
-  def write_defs(stream)
-    super
-    stream << %$
-      void #{test!}(void) {
-        #{element.type} e1, e2, e3;
-        #{type} c1, c2;
-        #{it} it;
-        
-        #{element.ctorEx}(&e1, -1);
-        #{element.ctorEx}(&e2, +1);
-        #{element.ctorEx}(&e3, 0);
-    
-        #{ctor}(&c1);
-        #{put}(&c1, e1, e3);
-        #{put}(&c1, e2, e3);
-        #{copy}(&c2, &c1);
-        
-        #{put}(&c1, e1, e2);
-        #{put}(&c2, e2, e1);
-    
-        {
-            int i;
-            #{element.type} e;
-            for(i = 0; i < 100; ++i) {
-                #{element.ctorEx}(&e, i);
-                #{put}(&c1, e, e);
-                #{element.dtor}(e);
-            }
-            for(i = 0; i < 100; i += 2) {
-                #{element.ctorEx}(&e, i);
-                #{remove}(&c1, e);
-                #{element.dtor}(e);
-            }
-            for(i = 1; i < 10; ++i) {
-                #{element.type} k;
-                #{element.ctorEx}(&k, i);
-                #{element.ctorEx}(&e, -i);
-                #{replace}(&c1, k, e);
-                #{element.dtor}(k);
-                #{element.dtor}(e);
-            }
-        }
-        
-        #{itCtor}(&it, &c1);
-        while(#{itMove}(&it)) {
-            #{element.type} k = #{itGetKey}(&it), e = #{itGetElement}(&it);
-            #{element.dtor}(k);
-            #{element.dtor}(e);
-        }
-    
-        #{element.dtor}(e1);
-        #{element.dtor}(e2);
-        #{element.dtor}(e3);
-    
-        #{equal}(&c1, &c2);
-        #{empty}(&c1);
-        #{size}(&c1);
-        
-        #{identify}(&c1);
-        #{identify}(&c2);
-    
-        #{purge}(&c1);
-        #{dtor}(&c1);
-        #{dtor}(&c2);
-      }
-    $
-  end
-end.new(:ValueTypeMap, ValueType, ValueType)
 
-CharString = Class.new(AutoC::String) do
-  def write_intf(stream)
-    super
-    stream << %$#{extern} void #{test!}(void);$
-  end
-  def write_defs(stream)
-    super
-    stream << %$
-      void #{test!}(void) {
-        #{type} s1, s2, s3;
-        #{ctor}(&s1, "z");
-        #{ctor}(&s2, "x");
-        #{pushInt}(&s1, 0);
-        #{copy}(&s3, &s1);
-        #{pushString}(&s3, &s2);
-        #{pushPtr}(&s3, -1);
-        printf("%s\\n", #{chars}(&s3));
-        #{dtor}(&s3);
-        #{dtor}(&s1);
-        #{dtor}(&s2);
-      }
-    $
-  end
-end.new(:String)
+def type_test(cls, *opts, &code)
+  t = Class.new(cls) do
+    def entities; super << Prologue end
+    attr_reader :tests
+    def initialize(*args)
+      super
+      @tests = []
+      @test_names = []
+    end
+    def setup(code = nil)
+      @setup_code = code
+    end
+    def cleanup(code = nil)
+      @cleanup_code = code
+    end
+    def test(name, code)
+      s = name.to_s
+      @test_names << [name, func_name = eval("test#{s[0,1].upcase}#{s[1..-1]}")]
+      @tests << %~
+        void #{func_name}(void) {
+          #{@setup_code}
+          #{code}
+          #{@cleanup_code}
+        }
+      ~
+    end
+    def write_defs(stream)
+      super
+      @tests.each {|f| stream << f}
+      stream << %~void #{runTests}(void) {~
+        stream << %~
+          fprintf(stdout, "+ %s\\n", "#{type}");
+          fflush(stdout);
+        ~
+        @test_names.each do |name, func_name|
+          stream << %~
+            run_test("#{name}", #{func_name});
+          ~
+        end
+      stream << %~
+        fputs("\\n", stdout); fflush(stdout);}
+      ~
+    end
+    def write_test_calls(stream)
+      stream << %$
+        #{runTests}();
+      $
+    end
+  end.new(*opts)
+  $tests << t
+  t.instance_eval(&code)
+end
 
+
+Dir["**/test_*.rb"].each {|t| load t}
+  
+  
 AutoC::Module.generate!(:Test) do |c|
-  c << ValueTypeVector
-  c << ValueTypeList
-  c << ValueTypeQueue
-  c << ValueTypeSet
-  c << ValueTypeMap
-  c << AutoC::HashMap.new(:IntStrMap, :int, "const char *")
-  c << ListIntSet
-  c << IntSet
-  c << AutoC::Vector.new(:PIntVector, PInt)
-  c << PInt << PValueType
-  c << AutoC::List.new(:ListPVectorValue, AutoC::Reference.new(AutoC::Vector.new(:PVectorValue, PValueType)))
-  c << CharString
+  c << Prologue
+  $tests.each {|t| c << t}
+  c << Epilogue
 end
