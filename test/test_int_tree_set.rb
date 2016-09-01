5e4b03f26cfa1e3bf3b0bb6f63b7370110254f09
fougas <fougas@localhost>
2016-09-01 19:03:51 +0500

    Fixed a bug in TreeSet#replace which prevented the HashSet's tests to be passed. Added the TreeSet iteration tests which seem to produce incorrect results.

--- test/test_int_tree_set.rb
+++ test/test_int_tree_set.rb
@@ -1,12 +1,24 @@
 type_test(AutoC::TreeSet, :IntTreeSet, :int) do
 
-setup %~#{type} t;~
+setup %~#{type} t; #{ctor}(&t);~
 cleanup %~#{dtor}(&t);~
 
 	test :create, %~
-		#{ctor}(&t);
 		TEST_TRUE( #{empty}(&t) );
 		TEST_EQUAL( #{size}(&t), 0 );
+	~
+
+	test :put, %~
+		TEST_EQUAL( #{size}(&t), 0 );
+		TEST_FALSE( #{contains}(&t, 1) );
+		TEST_TRUE( #{put}(&t, 1) );
+		TEST_TRUE( #{contains}(&t, 1) );
+		TEST_FALSE( #{put}(&t, 1) );
+		TEST_EQUAL( #{size}(&t), 1 );
+		TEST_TRUE( #{put}(&t, -1) );
+		TEST_TRUE( #{put}(&t, 2) );
+		TEST_EQUAL( #{size}(&t), 3 );
+		TEST_FALSE( #{contains}(&t, 0) );
 	~
 
 setup %~
@@ -33,11 +45,15 @@ cleanup %~
 	~
 
 setup %~
-	int values[] = {1,2,5,6,7,4,3,1,0};
-	int* p = &values;
+	int values[] = {-6,7,1,2,5,7,4,3,1,-6,0};
+	int* p = values;
 	#{type} t;
 	#{ctor}(&t);
-	while(*p++) #{put}(&t, *p);
+	while(*p != 0) {
+		#{put}(&t, *p);
+		TEST_TRUE( #{contains}(&t, *p) );
+		++p;
+	}
 ~
 cleanup %~
 	#{dtor}(&t);
@@ -47,4 +63,38 @@ cleanup %~
 		TEST_EQUAL( #{size}(&t), 7 );
 	~
 
+	test :contains, %~
+		TEST_TRUE( #{contains}(&t, 1) );
+		TEST_FALSE( #{contains}(&t, -1) );
+	~
+
+	test :iterateAscending, %~
+		int cur = 0, pre = 0, start = 1;
+		#{it} it;
+		#{itCtorEx}(&it, &t, 1);
+		while(#{itMove}(&it)) {
+			cur = #{itGet}(&it);
+			if(start) {
+				start = 0;
+			} else {
+				TEST_TRUE( pre < cur );
+				pre = cur;
+			}
+		}
+	~
+
+	test :iterateDescending, %~
+		int cur = 0, pre = 0, start = 1;
+		#{it} it;
+		#{itCtorEx}(&it, &t, 0);
+		while(#{itMove}(&it)) {
+			cur = #{itGet}(&it);
+			if(start) {
+				start = 0;
+			} else {
+				TEST_TRUE( pre > cur );
+				pre = cur;
+			}
+		}
+	~
 end
