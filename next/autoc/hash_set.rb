5e26b43d6b017e7c4a48ce530a19d9855e4e7447
Oleg A. Khlybov <fougas@mail.ru>
2021-09-14 15:57:08 +0500

    Fixed method naming & ependent entities ordering bugs.

--- next/autoc/hash_set.rb
+++ next/autoc/hash_set.rb
@@ -17,16 +17,18 @@ module AutoC
       @range = Range.new(self, visibility)
       @bucket = Bucket.new(self, element)
       @buckets = Buckets.new(self, @bucket)
-      @initial_dependencies << range << @buckets << @bucket
+      dependencies << range << @bucket << @buckets
       [@size, @empty].each(&:inline!)
       @compare = nil # Don't know how to order the vectors
       @manager = { minimum_capacity: 8, load_factor: 0.75, expand_factor: 1.5 }
     end
 
-    def composite_declarations(stream)
+    def canonic_tag = "HashSet<#{element.type}>"
+
+    def composite_interface_declarations(stream)
       stream << %$
         /**
-         * #{@defgroup} #{type} HashSet<#{element.type}> :: hash-based collection of unique values
+         * #{@defgroup} #{type} #{canonic_tag} :: hash-based unordered collection of elements of type #{element.type}
          * @{
          */
         typedef struct {
@@ -39,7 +41,7 @@ module AutoC
       stream << '/** @} */'
     end
 
-    def composite_definitions(stream)
+    def composite_interface_definitions(stream)
       stream << %$
         /**
          * #{@addtogroup} #{type}
@@ -119,10 +121,10 @@ module AutoC
         super
       end
 
-      def composite_declarations(stream)
+      def composite_interface_declarations(stream)
         stream << %$
           /**
-           * #{@defgroup} #{type} Range iterator for <#{iterable.type}> iterable container
+           * #{@defgroup} #{type} #{canonic_tag} :: range iterator for the iterable container #{iterable.canonic_tag}
            * @{
            */
           typedef struct {
@@ -133,7 +135,7 @@ module AutoC
         stream << '/** @} */'
       end
 
-      def composite_definitions(stream)
+      def composite_interface_definitions(stream)
         stream << %$
           /**
            * #{@addtogroup} #{type}
@@ -171,7 +173,7 @@ module AutoC
 
     def initialize(set, element) = super(Once.new { "_#{set.type}Bucket" }, element, :internal)
 
-    def declarations(stream)
+    def forward_declarations(stream)
       super
       stream << %$
         #{declare} void #{_adopt}(#{ptr_type} self, #{element.const_type} value);
@@ -184,7 +186,7 @@ module AutoC
       stream << %$
         /* Push value to the list bypassing the element's copy function */
         #{define} void #{_adopt}(#{ptr_type} self, #{element.const_type} value) {
-          /* Derived from ?push() */
+          /* Derived from #{push}() */
           #{node}* new_node = #{memory.allocate(node)};
           new_node->next_node = self->head_node;
           self->head_node = new_node;
@@ -193,7 +195,7 @@ module AutoC
         }
         /* Free the storage without calling the element's destructors */
         #{define} void #{_dispose}(#{ptr_type} self) {
-          /* Derived from ?drop() */
+          /* Derived from #{drop}() */
           while(!#{empty}(self)) {
             #{node}* this_node = self->head_node; assert(this_node);
             self->head_node = self->head_node->next_node;
@@ -211,14 +213,14 @@ module AutoC
 
     def initialize(set, bucket) = super(Once.new { "_#{set.type}Buckets" }, bucket, :internal)
 
-      def declarations(stream)
-        super
-        stream << %$
-          #{declare} void #{_dispose}(#{ptr_type} self);
-        $
-      end
+    def forward_declarations(stream)
+      super
+      stream << %$
+        #{declare} void #{_dispose}(#{ptr_type} self);
+      $
+    end
 
-      def definitions(stream)
+    def definitions(stream)
       super
       stream << %$
         /* Free the storage disposing the elements in turn */
