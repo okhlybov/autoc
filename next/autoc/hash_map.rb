3065e6751623e89a5116a75cbaa9e2f5e58ede0b
Oleg A. Khlybov <fougas@mail.ru>
2021-10-18 00:48:48 +0500

    Seeded the hash map type & unit test.

---
+++ next/autoc/hash_map.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+
+require 'autoc/container'
+require 'autoc/hash_set'
+
+
+module AutoC
+
+
+  class HashMap < AssociativeContainer
+
+    def initialize(type, key, element, visibility = :public)
+      super
+    end
+  end
+
+
+end
---
+++ test/test_char_int_hash_map.rb
@@ -0,0 +1,5 @@
+require 'autoc/hash_map'
+
+type_test(AutoC::HashMap, :CharIntHashMap, :char, :int) do
+  
+end
