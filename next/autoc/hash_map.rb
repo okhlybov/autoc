51ee280c747f53b7239653c3e6b4e6bc4b4a17c1
Oleg A. Khlybov <fougas@mail.ru>
2021-10-18 17:22:31 +0500

    Made some work on hash map type.

--- next/autoc/hash_map.rb
+++ next/autoc/hash_map.rb
@@ -12,8 +12,51 @@ module AutoC
 
     def initialize(type, key, element, visibility = :public)
       super
+      @node = Node.new(self, self.key, self.element)
+      @set = Set.new(self, @node)
+      dependencies << @node << @set
+    end
+
+    def canonic_tag = "HashMap<#{key.type} -> #{element.type}>"
+
+    def composite_interface_declarations(stream)
+      stream << %$
+        /**
+          * #{@defgroup} #{type} #{canonic_tag} :: hash-based unordered #{key.type} to #{element.type} mapping
+          * @{
+          */
+        typedef struct {
+          #{@set.type} set;
+        } #{type};
+      $
+      super
+      stream << '/** @} */'
     end
   end
 
 
+  class HashMap::Set < HashSet
+    def initialize(map, element) = super(Once.new { "_#{map.type}Set" }, element, :internal)
+  end
+
+
+  class HashMap::Node < Composite
+
+    def initialize(map, key, element)
+      super(Once.new { "_#{map.type}Node" }, :internal)
+      @key = key
+      @element = element
+      dependencies << @key << @element
+    end
+
+    def composite_interface_declarations(stream)
+      stream << %$
+        typedef struct {
+          #{@key.type} key;
+          #{@element.type} element;
+        } #{type};
+      $
+      super
+    end
+  end
 end
