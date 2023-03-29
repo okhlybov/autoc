7d690921a01decb2d193f80b2375443a7815dec0
Oleg A. Khlybov <fougas@mail.ru>
2023-03-29 13:16:57 +0500

    Move identifier decorators to a specific module.

--- lib/autoc/composite.rb
+++ lib/autoc/composite.rb
@@ -95,40 +95,41 @@ module AutoC
 
     self.hasher = Hasher.instance # Default cycle-xor hasher
 
-    # Pluggable CamelCase identifier decorator
-    CAMEL_CASE_DECORATOR = -> (type, symbol, abbreviate: false, **kws) {
-      id = symbol.to_s.sub(/[!?]$/, '') # Strip trailing !?
-      _ = # Check for leading underscore
-        if /^(_+)(.*)/ =~ id
-          id = Regexp.last_match(2) # Chop leading underscore
-          true
-        else
-          false
-        end
-      id = id[0] if abbreviate
-      # Convert _separated_names to the CamelCase
-      id = type.prefix + id.split('_').collect{ |s| s[0].upcase << s[1..-1] }.join
-      # Carry over the method name's leading underscore only if the prefix is not in turn underscored
-      _ && !type.prefix.start_with?('_') ? Regexp.last_match(1) + id : id
-    }
+    module Decorator
+      # Pluggable CamelCase identifier decorator
+      CAMEL_CASE = -> (type, symbol, abbreviate: false, **kws) {
+        id = symbol.to_s.sub(/[!?]$/, '') # Strip trailing !?
+        _ = # Check for leading underscore
+          if /^(_+)(.*)/ =~ id
+            id = Regexp.last_match(2) # Chop leading underscore
+            true
+          else
+            false
+          end
+        id = id[0] if abbreviate
+        # Convert _separated_names to the CamelCase
+        id = type.prefix + id.split('_').collect{ |s| s[0].upcase << s[1..-1] }.join
+        # Carry over the method name's leading underscore only if the prefix is not in turn underscored
+        _ && !type.prefix.start_with?('_') ? Regexp.last_match(1) + id : id
+      }
+      # Pluggable _snake_case identifier decorator
+      SNAKE_CASE = -> (type, symbol, abbreviate: false, **kws) {
+        id = symbol.to_s.sub(/[!?]$/, '') # Strip trailing !?
+        # Check for leading underscore
+        _ =
+          if /^(_+)(.*)/ =~ id
+            id = Regexp.last_match(2)
+            true
+          else
+            false
+          end
+        id = abbreviate ? "#{type.prefix}#{id[0]}" : "#{type.prefix}_#{id}"
+        # Carry over the method name's leading underscore only if the prefix is not in turn underscored
+        _ && !type.prefix.start_with?('_') ? Regexp.last_match(1) + id : id
+      }
+    end # Decorator
 
-    # Pluggable _snake_case identifier decorator
-    SNAKE_CASE_DECORATOR = -> (type, symbol, abbreviate: false, **kws) {
-      id = symbol.to_s.sub(/[!?]$/, '') # Strip trailing !?
-      # Check for leading underscore
-      _ =
-        if /^(_+)(.*)/ =~ id
-          id = Regexp.last_match(2)
-          true
-        else
-          false
-        end
-      id = abbreviate ? "#{type.prefix}#{id[0]}" : "#{type.prefix}_#{id}"
-      # Carry over the method name's leading underscore only if the prefix is not in turn underscored
-      _ && !type.prefix.start_with?('_') ? Regexp.last_match(1) + id : id
-    }
-
-    self.decorator = CAMEL_CASE_DECORATOR
+    self.decorator = Decorator::CAMEL_CASE
 
   private
     
