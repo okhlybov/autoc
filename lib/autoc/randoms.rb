c3bbd06495dc2abbc41b852e4c9d6f1124c7022c
Oleg A. Khlybov <fougas@mail.ru>
2023-05-12 19:18:56 +0500

    Introduce basic PRNG.

--- lib/autoc/randoms.rb
+++ lib/autoc/randoms.rb
@@ -18,7 +18,7 @@ module AutoC
     def self.seeder=(s) @seeder = s end
 
     def initialize
-      dependencies << Module::DEFINITIONS << DEFINITIONS
+      dependencies << Module::DEFINITIONS << Random::DEFINITIONS
       references << self.class.seeder
     end
 
@@ -28,7 +28,7 @@ module AutoC
         #ifndef AUTOC_SEED
           #define _AUTOC_RANDOMIZE_SEED
           #define AUTOC_SEED _autoc_seed /**< @brief Statically initialized (random) seed value */
-          AUTOC_EXTERN autoc_seed_t _autoc_seed; /**< @private */
+          AUTOC_EXTERN autoc_random_t _autoc_seed; /**< @private */
         #elif ~(~AUTOC_SEED + 1) == 1 /* if macro value is unspecified on the command line it is implicitly set to 1 */
           #undef AUTOC_SEED
           #define AUTOC_SEED 1
@@ -40,7 +40,7 @@ module AutoC
       super
       stream << %{
         #ifdef _AUTOC_RANDOMIZE_SEED
-          autoc_seed_t _autoc_seed = 1;
+          autoc_random_t _autoc_seed = 1;
           #if defined(__cplusplus)
             extern "C" void _autoc_seed_randomize(void);
           #elif defined(__GNUC__) || defined(__clang__) || defined(__INTEL_COMPILER) || defined(__INTEL_LLVM_COMPILER)
@@ -72,12 +72,14 @@ module AutoC
       }
     end
 
-    DEFINITIONS = Code.new interface: %{
-      #include <stddef.h>
-      typedef size_t autoc_seed_t;
-    }
-
   end # Seed
+
+
+  module Random
+    DEFINITIONS = Code.new interface: %{
+      typedef unsigned int autoc_random_t;
+    }
+  end
 
 
   class Seeder
@@ -86,7 +88,7 @@ module AutoC
 
     include Entity
 
-    def initialize = dependencies << Seed::DEFINITIONS
+    def initialize = dependencies << Random::DEFINITIONS
 
     def next = 'autoc_seeder_next()'
 
@@ -94,25 +96,27 @@ module AutoC
       super
       stream << %{
         /**
-          @brief Default seeder built upon a system timer
+          @brief Default random value seeder
 
           The exact quality depends on the target platform.
-          It is not meant to be cryptographically viable.
+          Its purpose is to be useful when no specific properties for the seeds are required.
+
+          As a consequence is not meant to be cryptographically viable.
 
           @since 2.1
         */
-        AUTOC_EXTERN autoc_seed_t autoc_seeder_next(void);
+        AUTOC_EXTERN autoc_random_t autoc_seeder_next(void);
       }
     end
 
     def render_implementation(stream)
       super
       stream << %{
-        #ifdef __cplusplus
+        #if __cplusplus >= 201103L
           #include <random>
         #endif
         #include <time.h>
-        autoc_seed_t autoc_seeder_next(void) {
+        autoc_random_t autoc_seeder_next(void) {
           #if defined(__POCC__)
             /* Pelles C check comes first as it might set _MSC_VER as well */
             unsigned r;
@@ -141,6 +145,57 @@ module AutoC
 
 
   Seed.seeder = Seeder.instance
+
+
+  class PRNG
+  
+    include Singleton
+
+    include Entity
+
+    def initialize = dependencies << Module::DEFINITIONS << Random::DEFINITIONS
+
+    def to_s = :autoc_random_t
+
+    def render_interface(stream)
+      super
+      stream << %{
+        /**
+          @brief Default pseudo-random number generator
+
+          @since 2.1
+        */
+        AUTOC_EXTERN autoc_random_t autoc_prng_next(autoc_random_t* state);
+      }
+    end
+
+    def render_implementation(stream)
+      super
+      # https://en.wikipedia.org/wiki/Lehmer_random_number_generator
+      stream << %{
+        autoc_random_t autoc_prng_next(autoc_random_t* state) {
+          /* basic Lehmer PRNG */
+          #if __cplusplus >= 201103L || __STDC_VERSION__ >= 199901L || defined(HAVE_LONG_LONG)
+            /* suitable for machines with types wider than autoc_random_t avaliable */
+            typedef unsigned long long int ull_t;
+            ull_t product = (ull_t)*state * 48271;
+            ull_t x = (product & 0x7fffffff) + (product >> 31);
+            x = (x & 0x7fffffff) + (x >> 31);
+            return *state = x;
+          #else
+            /* fallback implementation for any 32-bit machine */
+            const autoc_random_t A = 48271;
+            autoc_random_t low  = (*state & 0x7fff) * A;
+            autoc_random_t high = (*state >> 15)    * A;
+            autoc_random_t x = low + ((high & 0xffff) << 15) + (high >> 16);
+            x = (x & 0x7fffffff) + (x >> 31);
+            return *state = x;
+          #endif
+        }
+      }
+    end
+
+  end # PRNG
 
 
 end
