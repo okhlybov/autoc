5ea71b48eb50699818d451e326e956c2c01fc90c
Oleg A. Khlybov <fougas@mail.ru>
2023-09-05 13:39:31 +0500

    Preserve trailing unserscores in composite identifiers.

--- lib/autoc/composite.rb
+++ lib/autoc/composite.rb
@@ -103,6 +103,10 @@ module AutoC
       # Pluggable CamelCase identifier decorator
       CAMEL_CASE = -> (type, symbol, abbreviate: false, **kws) {
         id = symbol.to_s.sub(/[!?]$/, '') # Strip trailing !?
+        # Preserve trailing underscores
+        /(.*?)(_*)$/.match(id)
+        id = Regexp.last_match(1)
+        trail = Regexp.last_match(2)
         _ = # Check for leading underscore
           if /^(_+)(.*)/ =~ id
             id = Regexp.last_match(2) # Chop leading underscore
@@ -114,11 +118,15 @@ module AutoC
         # Convert _separated_names to the CamelCase
         id = type.prefix + id.split('_').collect{ |s| s[0].upcase << s[1..-1] }.join
         # Carry over the method name's leading underscore only if the prefix is not in turn underscored
-        _ && !type.prefix.start_with?('_') ? Regexp.last_match(1) + id : id
+        (_ && !type.prefix.start_with?('_') ? Regexp.last_match(1) + id : id) + trail
       }
       # Pluggable _snake_case identifier decorator
       SNAKE_CASE = -> (type, symbol, abbreviate: false, **kws) {
         id = symbol.to_s.sub(/[!?]$/, '') # Strip trailing !?
+        # Preserve trailing underscores
+        /(.*?)(_*)$/.match(id)
+        id = Regexp.last_match(1)
+        trail = Regexp.last_match(2)
         # Check for leading underscore
         _ =
           if /^(_+)(.*)/ =~ id
@@ -129,7 +137,7 @@ module AutoC
           end
         id = abbreviate ? "#{type.prefix}#{id[0]}" : "#{type.prefix}_#{id}"
         # Carry over the method name's leading underscore only if the prefix is not in turn underscored
-        _ && !type.prefix.start_with?('_') ? Regexp.last_match(1) + id : id
+        (_ && !type.prefix.start_with?('_') ? Regexp.last_match(1) + id : id) + trail
       }
     end # Decorator
 
