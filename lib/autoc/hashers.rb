d3b8bd454f232cd24a0fa8709cbf55f8c0098e2c
Oleg A. Khlybov <fougas@mail.ru>
2023-10-17 18:47:14 +0500

    Add integer hash functions.

--- lib/autoc/hashers.rb
+++ lib/autoc/hashers.rb
@@ -35,4 +35,48 @@ module AutoC
   }
 
 
+  class Hashers
+
+    include Singleton
+
+    include AutoC::Entity
+
+    def initialize = dependencies << Module::DEFINITIONS
+
+    def render_interface(stream)
+      stream << %{
+        /**
+          @brief General purpose hasher for int value
+
+          @since 2.1
+        */
+        AUTOC_INLINE int autoc_hash_int(int key) {
+          int c2=0x27d4eb2d;
+          key = (key ^ 61) ^ (key >> 16);
+          key = key + (key << 3);
+          key = key ^ (key >> 4);
+          key = key * c2;
+          key = key ^ (key >> 15);
+          return key;
+        }
+        /**
+          @brief General purpose hasher for long value
+
+          @since 2.1
+        */
+        AUTOC_INLINE long autoc_hash_long(long key) {
+          key = (~key) + (key << 21);
+          key = key ^ (key >> 24);
+          key = (key + (key << 3)) + (key << 8);
+          key = key ^ (key >> 14);
+          key = (key + (key << 2)) + (key << 4);
+          key = key ^ (key >> 28);
+          key = key + (key << 31);
+          return key;
+        }
+      }
+    end
+
+  end
+
 end
