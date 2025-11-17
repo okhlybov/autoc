e76f856368273543151a370654e7e1810580e1aa
Oleg A. Khlybov <fougas@mail.ru>
2025-11-17 21:38:37 +0500

    Fix incorrect MSVC detection macro

--- lib/autoc/primitive.rb
+++ lib/autoc/primitive.rb
@@ -191,7 +191,7 @@ class Complex < Primitive
       using autoc_long_double_complex_t = std::complex<long double>;
       using autoc_long_complex_t = autoc_long_double_complex_t;
     #else
-      #if defined(_MSC_VER) && !defined(__clang__) && !defined(__INTEL_COMPILER) && !defined(__INTEL_LLVM_COMPILER)
+      #if defined(_MSC_VER) && (!defined(__clang__) || !defined(__INTEL_COMPILER) || !defined(__INTEL_LLVM_COMPILER))
         #error Visual Studio requires C++ compilation mode for complex numeric types
       #endif
       typedef float complex autoc_float_complex_t;
