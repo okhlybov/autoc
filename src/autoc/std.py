bf063f7af3ee519862a0b089fa2e7a0e4d8e45cd
Oleg A. Khlybov <fougas@mail.ru>
2025-12-18 17:03:15 +0500

    Port of the Ruby  autoc/std code

---
+++ src/autoc/std.py
@@ -0,0 +1,96 @@
+from autoc.core import Primitive
+from autoc.module import SystemHeader, Code
+
+math_h = SystemHeader("math.h")
+tgmath_h = SystemHeader("tgmath.h")
+limits_h = SystemHeader("limits.h")
+assert_h = SystemHeader("assert.h")
+stdio_h = SystemHeader("stdio.h")
+stddef_h = SystemHeader("stddef.h")
+malloc_h = SystemHeader("malloc.h")
+string_h = SystemHeader("string.h")
+stdbool_h = SystemHeader("stdbool.h")
+complex_h = SystemHeader("complex.h")
+inttypes_h = SystemHeader("inttypes.h")
+
+stdlib_h = Code(interface="""
+  #define _CRT_RAND_S
+  #include <stdlib.h>
+""")
+
+bool = Primitive.register("_Bool", matcher=r"^(bool|_Bool)$", dependencies=[stdbool_h])
+
+char = Primitive.register("char")
+signed_char = Primitive.register("signed char", matcher=r"^signed\s+char$")
+unsigned_char = Primitive.register("unsigned char", matcher=r"^unsigned\s+char$")
+
+wchar_t = Primitive.register("wchar_t", dependencies=[stddef_h])
+
+short = signed_short = short_int = signed_short_int = Primitive.register("short", matcher=r"^(signed\s+)?short(\s+int)?$")
+unsigned_short = unsigned_short_int = Primitive.register("unsigned short", matcher=r"^unsigned\s+short(\s+int)?$")
+
+int = signed = signed_int = Primitive.register("int", matcher=r"^(int|signed|signed\s+int)$")
+unsigned = unsigned_int = Primitive.register("unsigned", matcher=r"^(unsigned|unsigned\s+int)$")
+
+long = signed_long = long_int = signed_long_int = Primitive.register("long", matcher=r"^(signed\s+)?long(\s+int)?$")
+unsigned_long = unsigned_long_int = Primitive.register("unsigned long", matcher=r"^unsigned\s+long(\s+int)?$")
+
+long_long = signed_long_long = long_long_int = signed_long_long_int = Primitive.register("long long", matcher=r"^(signed\s+)?long\s+long(\s+int)?$")
+unsigned_long_long = unsigned_long_long_int = Primitive.register("unsigned long long", matcher=r"^unsigned\s+long\s+long(\s+int)?$")
+
+size_t = Primitive.register("size_t", dependencies=[stddef_h])
+ptrdiff_t = Primitive.register("ptrdiff_t", dependencies=[stddef_h])
+uintptr_t = Primitive.register("uintptr_t", dependencies=[stddef_h])
+
+float = Primitive.register("float")
+double = Primitive.register("double")
+long_double = Primitive.register("long double", matcher=r"^long\s+double$")
+
+float_t = Primitive.register("float_t", dependencies=[math_h])
+double_t = Primitive.register("double_t", dependencies=[math_h])
+
+# Complex type definitions
+_complex_definitions = Code(
+  dependencies=[complex_h, tgmath_h],
+  interface="""
+    #ifdef __cplusplus
+      using autoc_double_complex_t = std::complex<double>;
+      using autoc_complex_t = autoc_double_complex_t;
+      using autoc_float_complex_t = std::complex<float>;
+      using autoc_long_double_complex_t = std::complex<long double>;
+      using autoc_long_complex_t = autoc_long_double_complex_t;
+    #else
+      #if defined(_MSC_VER) && (!defined(__clang__) || !defined(__INTEL_COMPILER) || !defined(__INTEL_LLVM_COMPILER) || !defined(__POCC__))
+        #error Visual Studio requires C++ compilation mode for complex numeric types
+      #endif
+      typedef float complex autoc_float_complex_t;
+      typedef double complex autoc_double_complex_t;
+      typedef autoc_double_complex_t autoc_complex_t;
+      typedef long double complex autoc_long_double_complex_t;
+      typedef autoc_long_double_complex_t autoc_long_complex_t;
+    #endif
+  """
+)
+
+class Complex(Primitive):
+  def __init__(self, name_c: str):
+    super().__init__(name_c, dependencies=[_complex_definitions])
+
+  def is_orderable(self): return False
+  def _compare(self, left, right): pass
+
+  def _hash(self, result, parameters, **kws): return Code(result, parameters, lambda source: f"(size_t)(creal({source})) ^ (size_t)(cimag({source}))", **kws)
+
+long_double_complex = Primitive.register("autoc_long_double_complex_t", matcher=r"^long\s+double\s+(complex|_Complex)$")
+double_complex = Primitive.register("autoc_double_complex_t", matcher=r"^double\s+(complex|_Complex)$")
+float_complex = Primitive.register("autoc_float_complex_t", matcher=r"^float\s+(complex|_Complex)$")
+complex = Primitive.register("autoc_complex_t", matcher=r"^(complex|_Complex)$")
+
+intptr_t = Primitive.register("intptr_t", dependencies=[inttypes_h])
+intmax_t = Primitive.register("intmax_t", dependencies=[inttypes_h])
+uintmax_t = Primitive.register("uintmax_t", dependencies=[inttypes_h])
+
+for bits in [8, 16, 32, 64]:
+  for prefix in ["int", "uint", "int_fast", "uint_fast", "int_least", "uint_least"]:
+    type_name = f"{prefix}{bits}_t"
+    globals()[type_name] = Primitive.register(type_name, dependencies=[inttypes_h])
