1869e9eae6c49f1aff6f616d2716ad3cd3a8db18
Oleg A. Khlybov <fougas@mail.ru>
2026-03-19 14:55:47 +0500

    Standard C memory manager

---
+++ src/autoc/memory.py
@@ -0,0 +1,20 @@
+import autoc.module
+import autoc.std as std
+import autoc.core as core
+
+
+# Standard C malloc()+free() memory manager
+class Manager(autoc.module.Code):
+  
+  def __init__(self):
+    super().__init__(dependencies=[std.malloc_h])
+    
+  def allocate(self, type=None, count=1, zero=False):
+    if type:
+      p = core.Pointer(type)
+      return f"({p})calloc({count}, sizeof({type}))" if zero else f"({p})malloc({count}*sizeof({type}))"
+    else:
+      return f"calloc({count}, 1)" if zero else f"malloc({count})"
+    
+  def free(self, obj):
+    return f"free({obj})"
