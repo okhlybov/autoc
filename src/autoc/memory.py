2a85d5ace5ac7ad6a7a3e67315607999d708f575
Oleg A. Khlybov <fougas@mail.ru>
2026-03-27 13:15:27 +0500

    Add cusom size option for memory mnager

--- src/autoc/memory.py
+++ src/autoc/memory.py
@@ -1,7 +1,6 @@
 import functools
+import autoc.std
 import autoc.module
-import autoc.std as std
-from autoc.core import Pointer
 
 
 # Generic C malloc()+free() memory manager
@@ -9,17 +8,16 @@ from autoc.core import Pointer
 class Manager(autoc.module.Code):
   
   def __init__(self):
-    super().__init__(dependencies=[std.malloc_h])
+    super().__init__(dependencies=[autoc.std.malloc_h])
     
-  def allocate(self, type=None, count=1, zero=False):
-    if type:
-      p = Pointer(type)
-      if zero:
-        return f"({p})calloc({count}, sizeof({type}))"
-      else:
-        return  f"({p})malloc(sizeof({type}))" if count == 1 else  f"({p})malloc({count}*sizeof({type}))"
+  def allocate(self, type=None, size=None, count=1, zero=False):
+    if not size:
+      size = f"sizeof({type})" if type else 1
+    if zero:
+      xalloc = f"calloc({count}, {size})"
     else:
-      return f"calloc({count}, 1)" if zero else f"malloc({count})"
+      xalloc = f"malloc({size})" if count == 1 else f"malloc({count}*{size})"
+    return f"({type}*){xalloc}" if type else xalloc
     
-  def free(self, obj):
-    return f"free({obj})"
+  def free(self, ptr):
+    return f"free({ptr})"
