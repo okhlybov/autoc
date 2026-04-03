2d608cd112903eb098d949e2d8ad7e3f593b6181
Oleg A. Khlybov <fougas@mail.ru>
2026-04-03 15:43:32 +0500

    Add target type cast opttion

--- src/autoc/memory.py
+++ src/autoc/memory.py
@@ -11,7 +11,7 @@ class Manager(autoc.module.Code):
   def __init__(self):
     super().__init__(dependencies=[autoc.std.malloc_h])
     
-  def allocate(self, element, count=1, zero=False):
+  def allocate(self, element, count=1, zero=False, cast=None):
     if isinstance(element, autoc.core.Type):
       type = element
       size = f"sizeof({type})"
@@ -22,7 +22,7 @@ class Manager(autoc.module.Code):
       code = f"calloc({count}, {size})"
     else:
       code = f"malloc({size})" if count == 1 else f"malloc({count}*{size})"
-    return f"({type}*){code}" if type else code
+    return f"({cast}*){code}" if cast else code
     
   def free(self, ptr):
     return f"free({ptr})"
