9cf451695f765135db0ea36fbba6f346926a2eb9
Oleg A. Khlybov <fougas@mail.ru>
2026-04-17 17:51:24 +0500

    Separate hashers for commutative and non-commutative data

--- src/autoc/hash/__init__.py
+++ src/autoc/hash/__init__.py
@@ -4,9 +4,9 @@ import autoc.std as std
 from autoc.core import out, Macro
 
 
-# Generic xor+shift hasher
+# Incremental xor+shift hasher for ordered data types
 @functools.cache
-class Hasher(autoc.module.Code):
+class XorShift(autoc.module.Code):
 
   state_t = std.size_t
   create = Macro(None, {"state": out(state_t)}, lambda state: f"{state} = 0") # TODO optional seed randomization
@@ -22,3 +22,17 @@ class Hasher(autoc.module.Code):
         return (value << 1) | (value >> (sizeof(size_t)*CHAR_BIT - 1));
       }}
     """)
+    
+    
+# Incremental xor hasher for unordered data types
+@functools.cache
+class Xor(autoc.module.Code):
+
+  state_t = std.size_t
+  create = Macro(None, {"state": out(state_t)}, lambda state: f"{state} = 0") # TODO optional seed randomization
+  destroy = Macro(None, {"state": out(state_t)}, lambda source: str())
+  update = Macro(None, {"state": out(state_t), "hash": std.size_t}, lambda state, hash: f"{state} ^= {hash}")
+  hash = Macro(std.size_t, {"state": state_t}, lambda state: state)
+  
+  def __init__(self):
+    super().__init__(dependencies=[self.state_t])
