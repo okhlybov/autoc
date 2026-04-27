a9ce1d6ebfcb4320b7308d5a122667d3b6306d7c
Oleg A. Khlybov <fougas@mail.ru>
2026-04-27 18:33:06 +0500

    Use with: for methods code

--- src/autoc/reference.py
+++ src/autoc/reference.py
@@ -5,8 +5,6 @@ from autoc.composite import Composite, _StructRenderer
 from functools import cached_property
 
 
-# FIXME use with:
-
 #
 class Reference(Composite, Pointer):
   
@@ -36,28 +34,28 @@ class Reference(Composite, Pointer):
 
   def __setup__(self):
 
-    self.new = self.method(self, "new", {}, linkage="INLINE")
-    result = Variable(self, "result")
-    self.new.code = f"""
-      {result.definition};
-      result = {self.memory.allocate(self.base)}; assert(result);
-      {self.base.create(result)};
-      return result;
-    """
+    with self.method(self, "new", {}) as f:
+      result = self.variable("result")
+      f.inline = f"""
+        {result.definition};
+        result = {self.memory.allocate(self.base)}; assert(result);
+        {self.base.create(result)};
+        return {result};
+      """
     
-    self.share = self.method(self, "share", {"target": inout(self)}, linkage="INLINE")
-    self.share.code = f"""
+    with self.method(self, "share", {"target": inout(self)}) as f:
+      f.inline = f"""
       assert(target);
       return target;
-    """
+      """
 
-    self.free = self.method(None, "free", {"target": inout(self)}, linkage="INLINE")
-    destroy = self.base.destroy(*self.free.arguments) if self.base.destructible else str()
-    self.free.code = f"""
-      assert(target);
-      {destroy};
-      {self.memory.free(*self.free.arguments)};
-    """
+    with self.method(None, "free", {"target": inout(self)}) as f:
+      destroy = self.base.destroy(f.target) if self.base.destructible else str()
+      f.inline = f"""
+        assert(target);
+        {destroy};
+        {self.memory.free(f.target)};
+      """
 
     super().__setup__()
 
@@ -66,58 +64,73 @@ class Reference(Composite, Pointer):
     return self.base.constructible
 
   def _create(self, result, parameters, **kws):
-    method = self.method(result, "create_", parameters, linkage="INLINE", visibility="PRIVATE", hidden=True, dependencies=[self.new], **kws)
-    method.code = f"*target = {self.new()};"
-    return method
+    with self.method(result, "create_", parameters, visibility="PRIVATE", hidden=True, **kws) as f:
+      f.inline = f"""
+        *target = {self.new()};
+      """
+    return f
   
   @property
   def destructible(self):
     return True
   
   def _destroy(self, result, parameters, **kws):
-    method = self.method(result, "destroy_", parameters, linkage="INLINE", visibility="PRIVATE", hidden=True, dependencies=[self.free], **kws)
-    method.code = f"{self.free(method.target)};"
-    return method
+    with self.method(result, "destroy_", parameters, visibility="PRIVATE", hidden=True, **kws) as f:
+      f.inline = f"""
+        assert(target);
+        {self.free(f.target)};
+      """
+    return f
 
   @property
   def copyable(self):
     return True
   
   def _copy(self, result, parameters, **kws):
-    method = self.method(result, "copy_", parameters, linkage="INLINE", visibility="PRIVATE", hidden=True, **kws)
-    method.code = f"""
-      assert(target);
-      assert(source);
-      *target = {method.source};
-    """
-    return method
+    with self.method(result, "copy_", parameters, visibility="PRIVATE", hidden=True, **kws) as f:
+      f.inline = f"""
+        assert(target);
+        assert(source);
+        *target = {f.source};
+      """
+    return f
 
   @property
   def comparable(self):
     return self.base.comparable
   
   def _equal(self, result, parameters, **kws):
-    method = self.method(result, "equal_", parameters, linkage="INLINE", visibility="PRIVATE", hidden=True, **kws)
-    method.code = f"return {self.base.equal(method.left, method.right)};"
-    return method
+    with self.method(result, "equal_", parameters, visibility="PRIVATE", hidden=True, **kws) as f:
+      f.inline = f"""
+        assert(left);
+        assert(right);
+        return {self.base.equal(f.left, f.right)};
+      """
+    return f
   
   @property
   def hashable(self):
     return self.base.hashable
   
   def _hash(self, result, parameters, **kws):
-    method = self.method(result, "hash_", parameters, linkage="INLINE", visibility="PRIVATE", hidden=True, **kws)
-    method.code = f"return {self.base.hash(method.target)};"
-    return method
+    with self.method(result, "hash_", parameters, visibility="PRIVATE", hidden=True, **kws) as f:
+      f.inline = f"""
+        return {self.base.hash(f.target)};
+      """
+    return f
 
   @property
   def orderable(self):
     return self.base.orderable
   
   def _compare(self, result, parameters, **kws):
-    method = self.method(result, "compare_", parameters, linkage="INLINE", visibility="PRIVATE", hidden=True, **kws)
-    method.code = f"return {self.base.compare(method.left, method.right)};"
-    return method
+    with self.method(result, "compare_", parameters, visibility="PRIVATE", hidden=True, **kws) as f:
+      f.inline = f"""
+        assert(left);
+        assert(right);
+        return {self.base.compare(f.left, f.right)};
+      """
+    return f
 
 
 #
@@ -138,33 +151,36 @@ class Shared(_StructRenderer, Reference):
   def __setup__(self):
     super().__setup__()
     
-    self.share.code = f"""
-      assert(target);
-      ++(({self._storage}*)target)->count;
-      return target;
-    """
-
-    result = Variable(self, "result")
-    self.new.code = f"""
-      {result.definition};
-      result = {self.memory.allocate(f"sizeof({self._storage})", cast=self.base)}; assert(result);
-      {self.base.create(result)};
-      (({self._storage}*)result)->count = 1;
-      return result;
-    """
+    with self.new as f:
+      result = self.variable("result")
+      f.inline = f"""
+        {result.definition};
+        result = {self.memory.allocate(f"sizeof({self._storage})", cast=self.base)}; assert(result);
+        {self.base.create(result)};
+        (({self._storage}*)result)->count = 1;
+        return result;
+      """
     
-    destroy = self.base.destroy(*self.free.arguments) if self.base.destructible else str()
-    self.free.code = f"""
+    with self.share as f:
+      f.inline = f"""
+        assert(target);
+        ++(({self._storage}*)target)->count;
+        return target;
+      """
+
+    with self.free as f:
+      destroy = self.base.destroy(f.target) if self.base.destructible else str()
+      f.inline = f"""
       assert(target);
       if(--(({self._storage}*)target)->count == 0) {{
         {destroy};
-        {self.memory.free(*self.free.arguments)};
+        {self.memory.free(f.target)};
       }}
     """
 
-    self.copy.dependencies = [self.share]
-    self.copy.code = f"""
-      assert(target);
-      assert(source);
-      *target = {self.share(self.copy.source)};
-    """
+    with self.copy as f:
+      f.inline = f"""
+        assert(target);
+        assert(source);
+        *target = {self.share(f.source)};
+      """
