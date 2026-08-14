ab63586a52db26d837cdded8021e4dd9420bb75d
Oleg A. Khlybov <fougas@mail.ru>
2026-08-15 01:15:30 +0500

    Basic type binding

--- src/autoc/core2.py
+++ src/autoc/core2.py
@@ -3,14 +3,14 @@ def _type(obj):
   match obj:
     case Type(): return obj
     case str(): return Primitive(obj)
-  raise f"{obj} is not convertable to Type"
+  raise TypeError(f"{obj} is not convertible to Type")
 
 
 #
 def _value(obj):
   match obj:
     case Value(): return obj
-  raise f"{obj} is not convertable to Value"
+  raise TypeError(f"{obj} is not convertible to Value")
 
 
 #
@@ -21,7 +21,10 @@ def _parameter(obj):
 
   
 #
-class Type: pass
+class Type:
+
+  def variable(self, name):
+    return Variable(self, name)
 
 
 #
@@ -105,10 +108,42 @@ class Indirection(Type):
   
 
 #
+class Statement:
+
+  def __init__(self, contents, *args, **kws):
+    super().__init__(*args, **kws)
+    self.contents = str(contents)
+
+  def __str__(self):
+    return self.contents
+
+
+def _indirection(obj):
+  return t.indirection if isinstance(t := _type(obj), Indirection) else 0
+
+
+def _indifference(lt, rt):
+  return _indirection(lt) - _indirection(rt)
+
+
+# Class representing a typed value of unspecified contents which can be passed to callable
 class Value:
   
-  def __init__(self, type):
+  def __init__(self, type, *args, **kws):
+    super().__init__(*args, **kws)
     self.type = _type(type)
+
+  def bind(self, type):
+    if (i := _indifference(self.type, type)) < 0:
+      raise ValueError(f"can not take address of {self} with & operator")
+    return "*"*i
+
+
+# Class representing a typed value with renderable contents
+class Expression(Value, Statement):
+
+  def bind(self, type):
+    return super().bind(type) + self.contents
 
 
 #
@@ -117,6 +152,12 @@ class Variable(Value):
   def __init__(self, type, name):
     super().__init__(type)
     self.name = str(name)
+
+  def bind(self, type):
+    i = _indifference(self.type, type)
+    if i < -1:
+      raise ValueError(f"too many & addressing operations requested for {self}")
+    return f"&{self.name}" if i == -1 else "*"*i + self.name
     
   @property
   def declaration_c(self):
@@ -147,6 +188,12 @@ class Callable:
   def signature_c(self):
     return "%s(%s)" % (self.result_c, ", ".join(str(t) for t in self.parameters.values()))
 
+  def _cast(self, contents):
+    if self.result is None:
+      return Statement(contents)
+    else:
+      return Expression(self.result, contents)
+
   class Parameter:
     def __init__(self, type):
       self.type = _type(type)
@@ -162,8 +209,9 @@ class Callable:
   class InOut(Parameter):
     def resolve(self, callable):
       return callable.resolve_inout(self.type)
-    
 
+
+#
 class Parametrized(Callable):
   
   def __init__(self, *args, **kws):
@@ -181,6 +229,7 @@ class Macro(Parametrized):
     super().__init__(result, parameters, **kws)
     self.emitter = emitter
 
+ 
   def resolve_in(self, type):
     return type.rvalue_type
   
@@ -189,7 +238,9 @@ class Macro(Parametrized):
   
   def resolve_inout(self, type):
     return type.rvalue_type
-  
+
+  def __call__(self, *arguments):
+    return self._cast("z")
   
 #
 class Functional(Parametrized):
