b2cb517a917ed9d9d32c185a13fdcbac85ac5153
Oleg A. Khlybov <fougas@mail.ru>
2026-08-20 17:11:11 +0500

    Port of string type

---
+++ src/autoc2/string.py
@@ -0,0 +1,222 @@
+import autoc2.std as std
+from autoc2.map import Map
+from autoc.module import Code
+from autoc2.range import DirectAccess
+from autoc2.core import inout, Indirection, Callable
+from autoc2.collection import _Range as CollectionRange
+
+
+#
+class String(Indirection, Map):
+  
+  def __init__(self, name, *args, **kws):
+    super().__init__("char", name, self, std.size_t, prefix=name, dependencies=(std.string_h, _static_code))
+    self.range = Range(self)
+
+  def __setup__(self):
+    super().__setup__()
+    self.create = self.macro_of("create", lambda target: f"{target} = (char*)_autoc_empty_string")
+    self.destroy = self.macro_of("destroy", lambda target: str(self.free(target)))
+    self.copy = self.macro_of("copy", lambda target, source: f"{target} = {self.new(source)}")
+
+    with self.method(self, "new", {"source": self}) as f:
+      f.inline_code = """
+        if(source) {
+          #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L
+            return strdup(source);
+          #elif defined(_MSC_VER)
+            return _strdup(source);
+          #else
+            size_t n;
+            char *s;
+            n = strlen(source)+1;
+            s = (char*)malloc(n); assert(s);
+            memcpy(s, source, n);
+            return s;
+          #endif
+        } else return (char*)_autoc_empty_string;
+      """
+      
+    with self.method(None, "free", {"target": inout(self)}) as f:
+      f.inline_code = f"""
+        assert(target);
+        if(target != _autoc_empty_string) free(target);
+      """
+    
+    with self.set as f:
+      f.inline_code = f"""
+        assert(target);
+        target[index] = element;
+      """
+      
+    with self.get as f:
+      f.inline_code = f"""
+        assert(target);
+        return target[index];
+      """
+
+    with self.view as f:
+      f.inline_code = f"""
+        assert(target);
+        return &target[index];
+      """
+
+    with self.indexed as f:
+      f.inline_code = f"""
+      assert(target);
+        return index < {self.size(f.target)};
+      """
+      
+    with self.size as f:
+      f.inline_code = f"""
+        assert(target);
+        return strlen(target);
+      """
+      
+    with self.empty as f:
+      f.inline_code = f"""
+        assert(target);
+        return !strlen(target);
+      """
+      
+    with self.contains as f:
+      f.inline_code = f"""
+        assert(target);
+        return strchr(target, element) != NULL;
+      """
+      
+    with self.method_of("equal") as f:
+      f.inline_code = f"""
+        assert(left);
+        assert(right);
+        return !strcmp(left, right);
+      """
+
+    with self.method_of("compare") as f:
+      f.inline_code = f"""
+        assert(left);
+        assert(right);
+        return strcmp(left, right);
+      """
+      
+    with self.method_of("hash") as f:
+      f.code = """
+        /* the djb2a algorithm */
+        char c;
+        size_t hash = 5381;
+        assert(target);
+        while((c = *target++)) {
+          c ^= (c << 5);
+          hash = ((hash << 5) + hash) + c;
+        }
+        return hash;
+      """
+
+  @property
+  def destructible(self):
+    return True
+      
+  #@cached_property
+  #def in_type(self):
+  #  return Indirection(self.base, constant=True)
+
+_static_code = Code(interface=f"""
+  /** @internal */
+  extern const char* _autoc_empty_string;
+""", implementation=f"""
+  const char* _autoc_empty_string = "";
+""")
+
+
+#
+class Range(CollectionRange, DirectAccess):
+  
+  def render_declarations(self, stream, header):
+    super().render_declarations(stream, header)
+    if header:
+      stream.append(f"""
+        typedef struct {{
+          {Indirection(self.iterable.type, constant=True)} iterable; /**< @private */
+          {self.iterable.index} front, back; /**< @private */
+        }} {self.name};
+      """)
+
+  def __setup__(self):
+    super().__setup__()
+
+    with self.method(Callable.Parameter(self), "new", {"iterable" : self.iterable}) as f:
+      f.inline_code = lambda: f"""
+        {self} result;
+        assert(iterable);
+        result.iterable = iterable;
+        result.front = 0;
+        result.back = {self.iterable.size("iterable")};
+        return result;
+      """
+
+    with self.empty as f:
+      f.inline_code = f"""
+        assert(target);
+        return target->front >= target->back;
+      """
+
+    with self.front as f:
+      f.inline_code = lambda: f"""
+        assert(target);
+        assert(!{self.empty(f.target)});
+        return {self.iterable.get("target->iterable", "target->front")};
+      """
+
+    with self.front_view as f:
+      f.inline_code = lambda: f"""
+        assert(target);
+        assert(!{self.empty(f.target)});
+        return {self.iterable.view("target->iterable", "target->front")};
+      """
+
+    with self.move_front as f:
+      f.inline_code = f"""
+        assert(target);
+        assert(!{self.empty(f.target)});
+        ++target->front;
+      """
+
+    with self.back as f:
+      f.inline_code = lambda: f"""
+        assert(target);
+        assert(!{self.empty(f.target)});
+        return {self.iterable.get("target->iterable", "target->back-1")};
+      """
+
+    with self.back_view as f:
+      f.inline_code = lambda: f"""
+        assert(target);
+        assert(!{self.empty(f.target)});
+        return {self.iterable.view("target->iterable", "target->back-1")};
+      """
+
+    with self.move_back as f:
+      f.inline_code = f"""
+        assert(target);
+        assert(!{self.empty(f.target)});
+        --target->back;
+      """
+
+    with self.get as f:
+      f.inline_code = lambda: f"""
+      assert(target);
+      return {self.iterable.get("target->iterable", "target->front + index")};
+    """
+
+    with self.view as f:
+      f.inline_code = lambda: f"""
+        assert(target);
+        return {self.iterable.view("target->iterable", "target->front + index")};
+      """
+
+    with self.size as f:
+      f.inline_code = f"""
+        assert(target);
+        assert(target->back >= target->front);
+        return target->back - target->front;
+      """  
